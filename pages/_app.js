b01472e4a2873218c2185b31d46c292b57f9173a
Yasin karaçam <yasinkaracam67@gmail.com>
2025-09-08 20:02:00 +0300

    Add @vercel/analytics package and integrate analytics component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css"
 import { Toaster } from "react-hot-toast"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Toaster position="top-right" />
+      <Analytics />
     </>
   )
 }
