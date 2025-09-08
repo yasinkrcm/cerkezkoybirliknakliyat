0bf2c386980554bb498344fd302e60f130ae9bce
Yasin karaçam <yasinkaracam67@gmail.com>
2025-09-08 18:12:33 +0300

    Add Google Tag Manager script for enhanced tracking

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17396834546"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17396834546');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
